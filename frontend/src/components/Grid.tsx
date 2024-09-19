968b25bb5620eab5c96d47902f3757f92395c238
minttuNB <cerealley@gmail.com>
2024-09-19 11:41:11 +0200

    Integrate in grid

--- frontend/src/components/Grid.tsx
+++ frontend/src/components/Grid.tsx
@@ -1,12 +1,28 @@
+import { useState } from "react";
 import Student from "./Student";
-import { GridProps } from "./types";
+import { GridProps, StudentProps } from "./types";
+import StudentCreationForm from "./StudentCreationForm";
 function Grid(props: GridProps) {
+	const [students, setStudents] = useState(props.elements);
+	function onAddStudent(event) {
+		event.preventDefault();
+		const stud: StudentProps = {
+			name: document.querySelector<HTMLInputElement>("#studentName")!.value,
+		};
+		console.log(stud);
+		setStudents(
+			students => [...students, stud]
+		);
+	}
 	return (
-		<section className="studentGrid">
-			{props.elements.map((student) => (
-				<Student name={student.name} />
-			))}
-		</section>
+		<>
+			<section className="studentGrid">
+				{students.map((student) => (
+					<Student name={student.name} />
+				))}
+			</section>
+			<StudentCreationForm onAddStudent={onAddStudent} />
+		</>
 	);
 }
 

ac9d764fbca21464aad5e37cb778b1ad1d33bd46
minttuNB <cerealley@gmail.com>
2024-09-19 12:33:11 +0200

    students api get endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,37 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+const students = [
+	{
+		id: 0,
+		name: "Anders",
+	},
+	{
+		id: 1,
+		name: "Espen",
+	},
+	{
+		id: 2,
+		name: "Ada",
+	},
+	{
+		id: 3,
+		name: "Aine",
+	},
+];
+const app = new Hono();
+app.use("*", cors());
 
-const app = new Hono()
-
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
-
-const port = 3000
-console.log(`Server is running on port ${port}`)
+app.get("/", (c) => {
+	return c.text("Hello Hono!");
+});
+app.get("/api/students", async (ctx) => {
+	ctx.json(JSON.stringify(students));
+});
+const port = 3999;
+console.log(`Server is running on port ${port}`);
 
 serve({
-  fetch: app.fetch,
-  port
-})
+	fetch: app.fetch,
+	port,
+});

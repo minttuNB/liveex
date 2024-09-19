6970fd98adcf2eaf9749996d585f11d6a997eb28
minttuNB <cerealley@gmail.com>
2024-09-19 13:09:56 +0200

    Delete button, not functional yet

--- frontend/src/components/Student.tsx
+++ frontend/src/components/Student.tsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
 import Avatar from "./Avatar";
 import { StudentProps } from "./types";
 
 function Student(props: StudentProps) {
+	const [hover, setHover] = useState(false);
+	let className = "delete";
+	if (hover) {
+		className = "delete visible";
+	}
 	return (
-		<article className="student">
-			<Avatar name={props.name}/>
+		<article
+			className="student"
+			onPointerEnter={() => setHover(true)}
+			onPointerLeave={() => setHover(false)}
+		>
+			<Avatar name={props.name} />
 			<h2>{props.name}</h2>
+			<button className={className}>X</button>
 		</article>
 	);
 }

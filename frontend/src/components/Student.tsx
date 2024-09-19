0d25102511c028215af2c2fe3b161fa862bd15d2
minttuNB <cerealley@gmail.com>
2024-09-19 11:01:00 +0200

    Simple styling

--- frontend/src/components/Student.tsx
+++ frontend/src/components/Student.tsx
@@ -3,10 +3,10 @@ import { StudentProps } from "./types";
 
 function Student(props: StudentProps) {
 	return (
-		<>
+		<article className="student">
 			<Avatar name={props.name}/>
 			<h2>{props.name}</h2>
-		</>
+		</article>
 	);
 }
 

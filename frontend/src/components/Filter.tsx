3436e7d2823a5e78aa8045c693c792a5de074f20
minttuNB <cerealley@gmail.com>
2024-10-10 13:31:02 +0200

    Skip intermediary variable

--- frontend/src/components/Filter.tsx
+++ frontend/src/components/Filter.tsx
@@ -1,11 +1,10 @@
 import { FilterProps } from "./types";
 
 export default function Filter(props: FilterProps) {
-	const filterValues = props.options;
 	return (
 		<select name="filter" id="filter" defaultValue="No filter" onChange={(event) => props.onFilterChange(event)}>
 			<option>No filter</option>
-			{filterValues.map((filterValue) => (
+			{props.options.map((filterValue) => (
 				<option key={filterValue} value={filterValue}>
 					{filterValue}
 				</option>

87e4490790d3a661b1d1ad1da850ba9971dda82d
minttuNB <cerealley@gmail.com>
2024-10-17 11:12:32 +0200

    Fetch students from backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,38 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Grid from "./components/Grid";
 import Total from "./components/Total";
 import { StudentProps } from "./components/types";
 import StudentCreationForm from "./components/StudentCreationForm";
 import StudentsFilter from "./components/Filter";
 import Student from "./components/Student";
-const studentsArray: StudentProps[] = [
-	{
-		id: "0",
-		name: "Anders",
-	},
-	{
-		id: "1",
-		name: "Espen",
-	},
-	{
-		id: "2",
-		name: "Ada",
-	},
-	{
-		id: "3",
-		name: "Aine",
-	},
-];
+import config from "./config";
 function App() {
-	const [students, setStudents] = useState(studentsArray ?? []);
+	const [students, setStudents] = useState<StudentProps[]>([]);
+	const [studentsFetchingError, setStudentsFetchingError] = useState("");
+	const [loading, setLoading] = useState(false);
 	const [currentFilter, setCurrentFilter] = useState("");
-	const filterOptions = [
-		...new Set(students.map((student: StudentProps) => student.name.slice(0, 1).toUpperCase())),
-	].sort();
+	async function fetchStudentData() {
+		try {
+			setLoading(true);
+			const response = await fetch(new URL(`${config.apiAddress}:${config.apiPort}/api/students`));
+			const studentsList: StudentProps[] = await response.json();
+			setLoading(false);
+			setStudents(studentsList);
+			setStudentsFetchingError("");
+		} catch (error) {
+			setStudentsFetchingError(`An error has occured while fetching the student list: ${error.message}`);
+		} finally {
+			setLoading(false);
+		}
+	}
+	useEffect(() => {
+		fetchStudentData();
+	}, []);
+	console.log(students);
+	const filterOptions =
+		students.length > 0
+			? [...new Set(students.map((student: StudentProps) => student.name.slice(0, 1).toUpperCase()))].sort()
+			: [];
 	function onAddStudent(event: React.FormEvent<HTMLFormElement>) {
 		event.preventDefault();
 		const stud: StudentProps = {
@@ -56,12 +60,23 @@ function App() {
 			<h2>Filter students list</h2>
 			<StudentsFilter options={filterOptions} onFilterChange={onFilterChange} />
 			<Grid>
-				{(currentFilter !== ""
-					? students.filter((student) => student.name.toUpperCase().startsWith(currentFilter))
-					: students
-				).map((student: StudentProps) => (
-					<Student key={student.id} name={student.name} id={student.id} onRemoveStudent={onRemoveStudent} />
-				))}
+				{loading ? (
+					<p>Loading...</p>
+				) : studentsFetchingError !== "" ? (
+					<p>{studentsFetchingError}</p>
+				) : (
+					(currentFilter !== ""
+						? students.filter((student) => student.name.toUpperCase().startsWith(currentFilter))
+						: students
+					).map((student: StudentProps) => (
+						<Student
+							key={student.id}
+							name={student.name}
+							id={student.id}
+							onRemoveStudent={onRemoveStudent}
+						/>
+					))
+				)}
 			</Grid>
 			<StudentCreationForm onAddStudent={onAddStudent} />
 		</>

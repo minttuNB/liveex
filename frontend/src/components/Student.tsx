e97d317919a9db943520ed4860dc2cce9e3a51a3
minttuNB <cerealley@gmail.com>
2024-10-17 12:27:39 +0200

    Define handler for changes in Student.tsx

--- frontend/src/components/Student.tsx
+++ frontend/src/components/Student.tsx
@@ -2,23 +2,30 @@ import { useState } from "react";
 import Avatar from "./Avatar";
 import { StudentProps } from "./types";
 
-function Student(props: StudentProps & {
-	onRemoveStudent?: (id: string) => void;
-}) {
+function Student(
+	props: StudentProps & {
+		onRemoveStudent?: (id: string) => void;
+		onChangeName?: (event: React.FocusEvent<HTMLHeadingElement>, id: string) => void;
+	}
+) {
 	const [hover, setHover] = useState(false);
 	let className = "delete";
 	if (hover) {
 		className = "delete visible";
 	}
 	return (
-		<article
-			className="student"
-			onPointerEnter={() => setHover(true)}
-			onPointerLeave={() => setHover(false)}
-		>
+		<article className="student" onPointerEnter={() => setHover(true)} onPointerLeave={() => setHover(false)}>
 			<Avatar name={props.name} />
-			<h2>{props.name}</h2>
-			<button className={className} onClick={()=>props.onRemoveStudent!(props.id)}>X</button>
+			<h2
+				contentEditable
+				suppressContentEditableWarning={true}
+				onBlur={(event) => props.onChangeName!(event, props.id)}
+			>
+				{props.name}
+			</h2>
+			<button className={className} onClick={() => props.onRemoveStudent!(props.id)}>
+				X
+			</button>
 		</article>
 	);
 }

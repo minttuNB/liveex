b119cd9cd20d5d9320333d5c7487a8c499609afd
minttuNB <cerealley@gmail.com>
2024-10-23 12:34:17 +0200

    needed to pass the other env

--- backend/src/db/db.ts
+++ backend/src/db/db.ts
@@ -1,4 +1,5 @@
 import Database from "better-sqlite3";
-import { env } from "process";
+import { env } from "../lib/env";
+console.log(env.DATABASE_URL);
 export const db = new Database(env.DATABASE_URL, { verbose: (str) => console.log(str) });
 export type DB = typeof db;

c291d05cce8a09bff9fe27e40d450c91c45f6d5c
minttuNB <cerealley@gmail.com>
2024-10-24 11:52:40 +0200

    add seeding as a setup phase

--- backend/src/db/setup.ts
+++ backend/src/db/setup.ts
@@ -1,6 +1,8 @@
 import { DB } from "./db";
+import { seed } from "./seed";
 import { createTables } from "./tables";
 
 export const setup = async (db: DB) => {
 	await createTables(db);
+	await seed(db);
 };

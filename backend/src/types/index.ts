1610e7ca50cc0f8b1d998338d612f1f2d6efd082
minttuNB <cerealley@gmail.com>
2024-10-24 11:51:51 +0200

    Add missing fields to student type

--- backend/src/types/index.ts
+++ backend/src/types/index.ts
@@ -2,6 +2,8 @@ import { UUID } from "crypto";
 type Student = {
 	id: UUID;
 	name: string;
+	createdAt?: Date;
+	updatedAt?: Date;
 };
 type Result<T> = { success: true; data: T } | { success: false; error: string };
 export type { Result, Student };

a34fc3f41a04f32d8f6e2171b3bd7dd70ce8a941
minttuNB <cerealley@gmail.com>
2024-10-24 11:52:11 +0200

    remove old stray log

--- backend/src/db/db.ts
+++ backend/src/db/db.ts
@@ -1,5 +1,4 @@
 import Database from "better-sqlite3";
 import { env } from "../lib/env";
-console.log(env.DATABASE_URL);
 export const db = new Database(env.DATABASE_URL, { verbose: (str) => console.log(str) });
 export type DB = typeof db;

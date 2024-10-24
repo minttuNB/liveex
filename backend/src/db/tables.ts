64c1762edcfc96dafa8eebe7d71fbf269ce47144
minttuNB <cerealley@gmail.com>
2024-10-24 11:52:01 +0200

    change data types to match better

--- backend/src/db/tables.ts
+++ backend/src/db/tables.ts
@@ -3,10 +3,10 @@ import { DB } from "./db";
 export const createTables = (db: DB) => {
 	db.exec(`
 		CREATE TABLE IF NOT EXISTS students (
-		  id TEXT NOT NULL PRIMARY KEY,
+		  id UUID NOT NULL PRIMARY KEY,
 		  name TEXT NOT NULL,
-		  created_at TEXT NOT NULL,
-		  updated_at TEXT NOT NULL
+		  created_at TIMESTAMP NOT NULL,
+		  updated_at TIMESTAMP NOT NULL
 		);
 	  `);
 };
